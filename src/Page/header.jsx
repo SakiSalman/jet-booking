94b3221cb78d6a23d1839d131565c8b5eed0f63e
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-04-08 03:22:33 +0600

    Refreshed complete file

--- src/Page/header.jsx
+++
@@ -1,4 +0,0 @@
-<footer>
-    <h1>this is footer</h1>
-    <h3>This is imran</h3>
-</footer>
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div>
+
+            {/* Start Coding from here */}
+
+
+
+    </div>
+  )
+}
+
+export default Home
