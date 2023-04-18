8c053f9caa43b6f0e2e1620551e791516a6db5ba
Imran Hasan Bipul <designerimranali@gmail.com>
2023-04-19 02:00:42 +0600

    another_page

--- src/Pages/Home/Deals/Deals.jsx
+++ src/Pages/Home/Deals/Deals.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Deals = () => {
   return (
-    <div>Deals</div>
+    <div>{/* Banner Start */}
+    <section className="py-52 flex items-center mt-[-90px] sm:mt-[-90px] text-white bg-no-repeat bg-cover" style={{ backgroundImage: "url(https://demo.curlythemes.com/private-jet/wp-content/uploads/sites/27/2018/03/header-random-1.jpg?id=231)" }}>
+       <div className="container mx-auto flex items-center justify-between flex-col sm:flex-row" >
+         <div className="content mx-auto">
+           <h1 className="text-3xl sm:text-6xl mx-auto text-center mt-5 text-white font-barlow uppercase"> Deals</h1>
+           <ul className='flex gap-4 justify-center mt-6'>
+             <li><a href="#">Home</a></li>
+             <li>-</li>
+             <li><a href="#">Deals</a></li>
+           </ul>
+         </div>
+       </div>
+     </section>
+{/* Banner End */}
+</div>
   )
 }
 
--- src/Pages/Home/Experience/Experience.jsx
+++ src/Pages/Home/Experience/Experience.jsx
@@ -2,7 +2,43 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div>
+    {/* Banner Start */}
+    <section className="py-52 flex items-center mt-[-90px] sm:mt-[-90px] text-white bg-no-repeat bg-cover" style={{ backgroundImage: "url(https://demo.curlythemes.com/private-jet/wp-content/uploads/sites/27/2018/03/header-random-1.jpg?id=231)" }}>
+       <div className="container mx-auto flex items-center justify-between flex-col sm:flex-row" >
+         <div className="content mx-auto">
+           <h1 className="text-3xl sm:text-6xl mx-auto text-center mt-5 text-white font-barlow uppercase"> Experience</h1>
+           <ul className='flex gap-4 justify-center mt-6'>
+             <li><a href="#">Home</a></li>
+             <li>-</li>
+             <li><a href="#">Experience</a></li>
+           </ul>
+         </div>
+       </div>
+     </section>
+    {/* Banner End */}
+
+    {/* Tour Packagees Start*/}
+    <div className="package">
+      <div className="container mx-auto">
+
+      </div>
+    </div>
+    {/* Tour Packagees End */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
   )
 }
 
