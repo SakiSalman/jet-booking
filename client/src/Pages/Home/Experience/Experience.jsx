376f7e763c78f9b0659caaa0d00f9f6e3766c167
Imran Hasan Bipul <designerimranali@gmail.com>
2023-05-02 22:33:54 +0600

    price package

--- client/src/Pages/Home/Experience/Experience.jsx
+++ client/src/Pages/Home/Experience/Experience.jsx
@@ -19,11 +19,102 @@ const Experience = () => {
     {/* Banner End */}
 
     {/* Tour Packagees Start*/}
+    <section className='py-24 bg-[#f5f3f1] bg-'>
     <div className="package">
-      <div className="container mx-auto">
-
-      </div>
-    </div>
+          <div className="container mx-auto">
+            <div className="card-box flex justify-between">
+              <div className="box p-4 mx-4 rounded-lg bg-white">
+                <img className='rounded-md' src="https://themeim.com/wp/flynext/wp-content/uploads/2021/11/15-570x400.jpg" alt="" />
+                <h3 className='text-3xl font-semibold pt-4'>Embraer P-300E</h3>
+                <h4 className='text-lg font-semibold py-4'>Legacy 600</h4>
+                <div className="price">
+                  <ul className='flex justify-between pb-3'>
+                    <li className='text-lg font-semibold'>8 - 14 Seats</li>
+                    <li className='text-lg font-semibold'>Price: $9,000/ hr</li>
+                  </ul>
+                </div>
+                <div>
+                  <hr />
+                </div>
+                <div className="card-footer flex justify-between pt-4">
+                  <div className="btnn">
+                  <a className='text-xl font-bold' href=""><i class="fa-sharp fa-solid fa-jet-fighter items-center"></i> Book Now</a> 
+                  </div>
+                  <div className="play">
+                  <span><a href="#"><i class="fa-sharp fa-solid fa-circle-play text-2xl"></i></a> </span>
+                  </div>
+                </div>
+              </div>
+              <div className="box p-4 mx-4 rounded-lg bg-white">
+                <img className='rounded-md' src="https://themeim.com/wp/flynext/wp-content/uploads/2021/11/ambulance-jet-inside-1-570x400.jpg" alt="" />
+                <h3 className='text-3xl font-semibold pt-4'>Embraer P-300E</h3>
+                <h4 className='text-lg font-semibold py-4'>Legacy 600</h4>
+                <div className="price">
+                  <ul className='flex justify-between pb-3'>
+                    <li className='text-lg font-semibold'>8 - 14 Seats</li>
+                    <li className='text-lg font-semibold'>Price: $9,000/ hr</li>
+                  </ul>
+                </div>
+                <div>
+                  <hr />
+                </div>
+                <div className="card-footer flex justify-between pt-4">
+                  <div className="btnn">
+                  <a className='text-xl font-bold' href=""><i class="fa-sharp fa-solid fa-jet-fighter items-center"></i> Book Now</a> 
+                  </div>
+                  <div className="play">
+                  <span><a href="#"><i class="fa-sharp fa-solid fa-circle-play text-2xl"></i></a> </span>
+                  </div>
+                </div>
+              </div>
+              <div className="box p-4 mx-4 rounded-lg bg-white">
+                <img className='rounded-md' src="https://themeim.com/wp/flynext/wp-content/uploads/2021/11/flight-attendant-good-looking-570x400.jpg" alt="" />
+                <h3 className='text-3xl font-semibold pt-4'>Embraer P-300E</h3>
+                <h4 className='text-lg font-semibold py-4'>Legacy 600</h4>
+                <div className="price">
+                  <ul className='flex justify-between pb-3'>
+                    <li className='text-lg font-semibold'>8 - 14 Seats</li>
+                    <li className='text-lg font-semibold'>Price: $9,000/ hr</li>
+                  </ul>
+                </div>
+                <div>
+                  <hr />
+                </div>
+                <div className="card-footer flex justify-between pt-4">
+                  <div className="btnn">
+                  <a className='text-xl font-bold' href=""><i class="fa-sharp fa-solid fa-jet-fighter items-center"></i> Book Now</a> 
+                  </div>
+                  <div className="play">
+                  <span><a href="#"><i class="fa-sharp fa-solid fa-circle-play text-2xl"></i></a> </span>
+                  </div>
+                </div>
+              </div>
+              <div className="box p-4 mx-4 rounded-lg bg-white">
+                <img className='rounded-md' src="https://themeim.com/wp/flynext/wp-content/uploads/2021/11/photo-1576725386266-5c4b8c63da66-570x400.jpg" alt="" />
+                <h3 className='text-3xl font-semibold pt-4'>Embraer P-300E</h3>
+                <h4 className='text-lg font-semibold py-4'>Legacy 600</h4>
+                <div className="price">
+                  <ul className='flex justify-between pb-3'>
+                    <li className='text-lg font-semibold'>8 - 14 Seats</li>
+                    <li className='text-lg font-semibold'>Price: $9,000/ hr</li>
+                  </ul>
+                </div>
+                <div>
+                  <hr />
+                </div>
+                <div className="card-footer flex justify-between pt-4">
+                  <div className="btnn">
+                  <a className='text-xl font-bold' href=""><i class="fa-sharp fa-solid fa-jet-fighter items-center"></i> Book Now</a> 
+                  </div>
+                  <div className="play">
+                  <span><a href="#"><i class="fa-sharp fa-solid fa-circle-play text-2xl"></i></a> </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
     {/* Tour Packagees End */}
 
 
