2a2e02007ed51e6145739ed67c38c93b9f3d2262
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-06-03 00:55:08 +0600

    add new jet controller done

---
+++ backend/controllers/jetControllers.js
@@ -0,0 +1,26 @@
+import { createError } from "../utility/customError.js";
+import Jets from '../models/Charter.js'
+/**
+ * @method POST
+ * @route "api/v1/user"
+ * @purpose GET ALL USERS
+ */
+
+export const registerJet = async (req, res, next) => {
+  try {
+    const { jet_name, routes, from, to, regular_price, sale_price, seat, featured_image } =
+      req.body;
+
+    // Check all fields are required
+    if ((!jet_name || !routes || !from || !to || !regular_price ||!sale_price || !seat || !featured_image ))
+      return next(createError(401, "All Fields Are Required!"));
+
+    // create a database collection for Jets
+    const jet = await Jets.create(
+      { ...req.body}
+    )
+    res.status(200).json(jet)
+  } catch (error) {
+    next(error);
+  }
+};
