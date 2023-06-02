12134dfa86031b40bae67158375c12e4e4399ea0
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-06-03 00:12:39 +0600

    create user controller updated

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -1,15 +1,12 @@
 
 import Users from "../models/Users.js"
+import { createError } from "../utility/customError.js";
 
 /**
- * @method POST
+ * @method GET
  * @route "api/v1/user"
  * @purpose GET ALL USERS
  */
-
-
-
-
 
 export const users = async (req, res, next) => {
 
@@ -21,10 +18,41 @@ export const users = async (req, res, next) => {
         
     }
 }
+
+/**
+ * @method POST
+ * @route "api/v1/user"
+ * @purpose GET ALL USERS
+ */
+
 export const registerUser = async (req, res, next) => {
 
     try {
-        const user = await Users.create(req.body)
+
+        const {first_name,
+        last_name,
+        email,
+        phone,
+        password} = req.body
+
+        if (!first_name, !last_name, !email, !phone, !password) {
+
+            return next(createError(401, 'All Fields Are Required!'))
+            
+        }
+
+        // get user with same email
+        const existingUser = await Users.findOne({email})
+
+        if (existingUser) {
+            return next(createError(401, 'Account Already Exist Please Login!'))
+
+        }
+
+        // Create Unique User
+        const user = await Users.create({
+            ...req.body,
+        })
 
     } catch (error) {
         
