a8b4d914315c4787768a068f2267d65d415b6e70
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-06-03 22:58:15 +0600

    compare password function done

--- backend/utility/hasPassword.js
+++ backend/utility/hasPassword.js
@@ -1,9 +1,19 @@
 import bcrypt from 'bcryptjs'
 
 
-
+// Has Password
 export const hasPass = async (pass) => {
 
     const hasPass = await bcrypt.hash(pass, 10)
     return hasPass
+}
+
+
+// Verify Password
+
+export const verifyPass = async (has, pass) => {
+
+    const verify = await bcrypt.compare(pass, has)
+
+    return verify
 }
